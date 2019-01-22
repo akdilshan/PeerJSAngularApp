19e97f7f6ae17769626491005688844fca536b76
akdilshan <dilshanamarasinghe1@gmail.com>
2019-01-22 15:31:36 +0800

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConferenceRoomComponent } from './conference-room/conference-room.component';
 import { FormsModule } from '@angular/forms';
 
-
-import {MatListModule, MatCardModule, MatDividerModule, MatGridListModule} from '@angular/material';
+import {MatListModule, MatCardModule, MatDividerModule, MatGridListModule, MatButtonModule, MatDialogModule, MatInputModule, MatChipsModule} from '@angular/material';
+import { MobileviewComponent } from './mobileview/mobileview.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
-    ConferenceRoomComponent
+    ConferenceRoomComponent, MobileviewComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
-    MatCardModule, MatDividerModule,MatListModule, MatGridListModule
+    MatCardModule, MatDividerModule,MatListModule, MatGridListModule, MatButtonModule, MatDialogModule, MatInputModule, MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
